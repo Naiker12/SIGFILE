5a64bc8fcd0faa8a8968590551029c39e6f9258d
naiker12 <gomeznaiker@gmail.com>
2025-06-22 18:14:38 -0500

    Fix 404 error - correct handler function

--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+import sys
+import os
 
-app = Flask(__name__, 
-           template_folder='../templates',
-           static_folder='../static')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+from app import app
 
 def handler(request):
     return app
+
+if __name__ == "__main__":
+    app.run(debug=True)
